c37d5f9498c0421f5accc49ae6e7a43f39ba0584
flwedu <edutraquino@gmail.com>
2021-08-04 12:56:34 -0300

    Update ShoppingCartController.java
    se o item ja estiver no carrinho, apenas atualiza seu amount

--- shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/controller/ShoppingCartController.java
+++ shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/controller/ShoppingCartController.java
@@ -1,7 +1,8 @@
 package one.digitalinnovation.projeto.shoppingcart.controller;
 
-import one.digitalinnovation.projeto.shoppingcart.model.Item;
+import one.digitalinnovation.projeto.shoppingcart.model.ItemInAList;
 import one.digitalinnovation.projeto.shoppingcart.model.ShoppingCart;
+import one.digitalinnovation.projeto.shoppingcart.repository.ItemInAListRepository;
 import one.digitalinnovation.projeto.shoppingcart.repository.ShoppingCartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,26 +17,35 @@ public class ShoppingCartController {
     @Autowired
     private ShoppingCartRepository shoppingCartRepository;
 
+    @Autowired
+    private ItemInAListRepository itemInAListRepository;
+
     @RequestMapping("/{id}")
     Optional<ShoppingCart> findById(@PathVariable Long id){
         return shoppingCartRepository.findById(id);
     }
 
     @PostMapping("/{id}")
-    ShoppingCart addItem(@PathVariable Long id, @RequestBody Item item){
+    ShoppingCart addItem(@PathVariable Long id, @RequestBody ItemInAList item){
 
+        // Primeiro persiste o item na base de dados
+        ItemInAList savedItem = itemInAListRepository.save(item);
+
+        // Depois verifica se já existe um carrinho com esse ID na URL.
         Optional<ShoppingCart> findShoppingCart = shoppingCartRepository.findById(id);
 
         // Se já existir um carrinho com esse ID, adiciona o item a ele
         if (findShoppingCart.isPresent()){
-            findShoppingCart.get().getItemList().add(item);
-            return findShoppingCart.get();
+            findShoppingCart.get().addItemToShoppingCart(savedItem);
+
+            // Retorna o carrinho atualizado após persisti-lo
+            return shoppingCartRepository.save(findShoppingCart.get());
         }
 
         // Se não existir ainda, então persista o carrinho com o ID passado e já adicione o item
         ShoppingCart newShoppingCart = ShoppingCart.builder()
                 .id(id)
-                .itemList(List.of(item))
+                .itemList(List.of(savedItem))
                 .build();
         return shoppingCartRepository.save(newShoppingCart);
     }
