fb0810632f0e1667b5bddf1b7e2dd044c7d9a7eb
flwedu <edutraquino@gmail.com>
2021-08-07 17:14:37 -0300

    Delete ItemInAListRepository.java

--- shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/repository/ItemInAListRepository.java
+++
@@ -1,9 +0,0 @@
-package one.digitalinnovation.projeto.shoppingcart.repository;
-
-import one.digitalinnovation.projeto.shoppingcart.model.ItemInAList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemInAListRepository extends JpaRepository<ItemInAList, Long> {
-}
