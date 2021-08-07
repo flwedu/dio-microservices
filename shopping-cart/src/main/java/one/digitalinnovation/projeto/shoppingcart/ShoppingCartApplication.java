8ea943c66106a5f66533e8b43ad92b55db3ecf2f
flwedu <edutraquino@gmail.com>
2021-08-07 14:17:57 -0300

    created RestTemplate instance recovery

--- shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/ShoppingCartApplication.java
+++ shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/ShoppingCartApplication.java
@@ -2,11 +2,21 @@ package one.digitalinnovation.projeto.shoppingcart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class ShoppingCartApplication {
+
+    // O RestTemplate permite fazer requisições e obter dados de outros serviços.
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+	return new RestTemplate();
+    }
 
     public static void main(String[] args) {
 	SpringApplication.run(ShoppingCartApplication.class, args);
