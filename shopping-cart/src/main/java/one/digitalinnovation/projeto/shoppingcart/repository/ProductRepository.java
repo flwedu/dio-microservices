e9f0f44c116b7132699cebf67994fcf7ad597f9a
flwedu <edutraquino@gmail.com>
2021-08-08 11:32:25 -0300

    add annotation @Repository

--- shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/repository/ProductRepository.java
+++ shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package one.digitalinnovation.projeto.shoppingcart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import one.digitalinnovation.projeto.shoppingcart.model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
