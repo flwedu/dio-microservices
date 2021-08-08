60f7791b072100aa67d05c45ed5d56137bd31576
flwedu <edutraquino@gmail.com>
2021-08-08 13:24:26 -0300

    rename and fix nullpointer exception

--- shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/model/Cart.java
+++ shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/model/Cart.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -23,17 +24,17 @@ import lombok.Setter;
 @Builder
 public class Cart {
 
-    @Id()
+    @Id
     private Long id;
 
-    @OneToMany
-    private List<Product> productList;
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Product> products = new ArrayList<Product>();
 
-    public List<Product> getProductList() {
-	if (productList == null) {
-	    productList = new ArrayList<>();
+    public List<Product> getProducts() {
+	if (products == null) {
+	    products = new ArrayList<Product>();
 	}
-	return productList;
+	return products;
     }
 
     // Esse método verifica se o item já existe na lista.
@@ -41,10 +42,10 @@ public class Cart {
     // Caso positivo, apenas ajusta o seu amount (quantidade).
     public void addProductToShoppingCart(Product newProduct) {
 
-	if (getProductList().stream().noneMatch(product -> product.getId() == newProduct.getId())) {
-	    getProductList().add(newProduct);
+	if (getProducts().stream().noneMatch(product -> product.getId() == newProduct.getId())) {
+	    getProducts().add(newProduct);
 	} else {
-	    getProductList().stream().filter(product -> product.getId() == newProduct.getId()).forEach(product -> {
+	    getProducts().stream().filter(product -> product.getId() == newProduct.getId()).forEach(product -> {
 		product.setAmount(product.getAmount() + newProduct.getAmount());
 	    });
 	}
