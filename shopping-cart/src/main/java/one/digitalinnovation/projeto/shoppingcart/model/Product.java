96f66b66fa8f9d368888083cf410318712f29c6e
flwedu <edutraquino@gmail.com>
2021-08-08 13:24:42 -0300

    ID is now a composite PK

--- shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/model/Product.java
+++ shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/model/Product.java
@@ -1,10 +1,13 @@
 package one.digitalinnovation.projeto.shoppingcart.model;
 
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,10 +20,21 @@ import lombok.Setter;
 @Builder
 public class Product {
 
-    @Id
-    private Long id;
+    @EmbeddedId
+    private Id id;
 
     private String name;
     private Long amount;
+
+    @Getter
+    @Setter
+    @EqualsAndHashCode
+    public static class Id implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Long productId;
+	private Long cartId;
+
+    }
 
 }
