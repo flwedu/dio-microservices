2224ed299228faee13e5fef4e5a27877c9a6d89f
flwedu <edutraquino@gmail.com>
2021-08-08 14:32:45 -0300

    classes desnecessárias removidas e modificação de atributos

--- shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/restclient/ProductRestClient.java
+++ shopping-cart/src/main/java/one/digitalinnovation/projeto/shoppingcart/restclient/ProductRestClient.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import one.digitalinnovation.projeto.shoppingcart.dao.ProductDAO;
 import one.digitalinnovation.projeto.shoppingcart.model.Product;
 import one.digitalinnovation.projeto.shoppingcart.repository.ProductRepository;
 
@@ -29,10 +28,10 @@ public class ProductRestClient {
 	this.productRepository = productRepository;
     }
 
-    public ResponseEntity<ProductDAO> getProductDAOById(Long id) {
+    public ResponseEntity<Product> getProductById(Long id) {
 
 	return clientRest.exchange("http://localhost:8080/product/" + id.toString(), HttpMethod.GET, null,
-		ProductDAO.class);
+		Product.class);
     }
 
     public Product findAndSave(Product product) {
