dbd767fb6070c46bb3d1c31542412d37e2c98e0b
flwedu <edutraquino@gmail.com>
2021-08-10 07:37:04 -0300

    add method to save an product array

--- product-catalog/src/main/java/one/digitalinnovation/projeto/productcatalog/controller/ProductController.java
+++ product-catalog/src/main/java/one/digitalinnovation/projeto/productcatalog/controller/ProductController.java
@@ -1,8 +1,5 @@
 package one.digitalinnovation.projeto.productcatalog.controller;
 
-import one.digitalinnovation.projeto.productcatalog.model.Product;
-import one.digitalinnovation.projeto.productcatalog.repository.ProductRepository;
-
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -15,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import one.digitalinnovation.projeto.productcatalog.model.Product;
+import one.digitalinnovation.projeto.productcatalog.repository.ProductRepository;
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
@@ -24,19 +24,24 @@ public class ProductController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     Product save(@RequestBody Product product) {
-        return productRepository.save(product);
+	return productRepository.save(product);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    Iterable<Product> saveAll(@RequestBody Iterable<Product> productList) {
+	return productRepository.saveAll(productList);
     }
 
     @GetMapping()
     ArrayList<Product> listAll() {
-        var listaProdutos = new ArrayList<Product>();
-        productRepository.findAll().forEach(listaProdutos::add);
+	var listaProdutos = new ArrayList<Product>();
+	productRepository.findAll().forEach(listaProdutos::add);
 
-        return listaProdutos;
+	return listaProdutos;
     }
 
     @GetMapping("/{id}")
     Optional<Product> findById(@PathVariable Long id) {
-        return productRepository.findById(id);
+	return productRepository.findById(id);
     }
 }
