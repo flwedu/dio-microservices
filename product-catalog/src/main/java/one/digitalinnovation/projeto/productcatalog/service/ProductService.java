245ce28f051924d2179a24479c610c403758e8a0
flwedu <edutraquino@gmail.com>
2021-08-11 21:16:00 -0300

    Create ProductService.java

---
+++ product-catalog/src/main/java/one/digitalinnovation/projeto/productcatalog/service/ProductService.java
@@ -0,0 +1,33 @@
+package one.digitalinnovation.projeto.productcatalog.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import one.digitalinnovation.projeto.productcatalog.model.Product;
+import one.digitalinnovation.projeto.productcatalog.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product save(Product product) {
+	return productRepository.save(product);
+    }
+
+    public Iterable<Product> saveAll(Iterable<Product> productList) {
+	return productRepository.saveAll(productList);
+    }
+
+    public Iterable<Product> findAll() {
+	return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+	return findById(id);
+    }
+
+}
