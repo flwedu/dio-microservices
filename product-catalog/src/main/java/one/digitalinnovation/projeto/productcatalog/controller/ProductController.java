09c4de4c0f657e4ffce36cd44a851e3f5ed4fdd4
flwedu <edutraquino@gmail.com>
2021-08-11 21:17:31 -0300

    controller now consumes a ProductService

--- product-catalog/src/main/java/one/digitalinnovation/projeto/productcatalog/controller/ProductController.java
+++ product-catalog/src/main/java/one/digitalinnovation/projeto/productcatalog/controller/ProductController.java
@@ -13,35 +13,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import one.digitalinnovation.projeto.productcatalog.model.Product;
-import one.digitalinnovation.projeto.productcatalog.repository.ProductRepository;
+import one.digitalinnovation.projeto.productcatalog.service.ProductService;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
     @Autowired
-    private ProductRepository productRepository;
+    private ProductService productService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     Product save(@RequestBody Product product) {
-	return productRepository.save(product);
+	return productService.save(product);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     Iterable<Product> saveAll(@RequestBody Iterable<Product> productList) {
-	return productRepository.saveAll(productList);
+	return productService.saveAll(productList);
     }
 
     @GetMapping()
     ArrayList<Product> listAll() {
 	var listaProdutos = new ArrayList<Product>();
-	productRepository.findAll().forEach(listaProdutos::add);
+	productService.findAll().forEach(listaProdutos::add);
 
 	return listaProdutos;
     }
 
     @GetMapping("/{id}")
     Optional<Product> findById(@PathVariable Long id) {
-	return productRepository.findById(id);
+	return productService.findById(id);
     }
 }
